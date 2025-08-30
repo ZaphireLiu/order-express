9a87f05e4e7b600d3dd9e6fe5d8d71847758890d
ZaphireLiu <liuhan.1631@gmail.com>
2025-08-30 23:04:04 +0800

    Skip/Untested: 微信支付逻辑

--- project-sky-admin-vue-ts/src/views/setmeal/components/AddDish.vue
+++ project-sky-admin-vue-ts/src/views/setmeal/components/AddDish.vue
@@ -168,7 +168,7 @@ export default class extends Vue {
   }
   // 添加菜品
   private checkedListHandle(value: [string]) {
-    // TODO 实现倒序 由于value是组件内封装无法从前面添加 所有取巧处理倒序添加
+    // NOTE 实现倒序 由于value是组件内封装无法从前面添加 所有取巧处理倒序添加
     // 倒序展示 - 数据处理前反正 为正序
     this.checkedListAll.reverse()
     // value 是一个只包含菜品名的数组 需要从 dishList中筛选出 对应的详情
