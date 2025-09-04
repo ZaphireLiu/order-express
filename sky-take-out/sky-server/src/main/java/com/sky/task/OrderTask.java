de761fb3797b65ca5cf7e596b3f0d51b4232f85e
ZaphireLiu <liuhan.1631@gmail.com>
2025-09-04 19:50:41 +0800

    Add: 定时任务

---
+++ sky-take-out/sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,77 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+/*
+  cron表达式其实就是一个字符串，通过cron表达式可以定义任务触发的时间
+  构成规则：分为6或7个域，由空格分隔开，每个域代表一个含义
+  每个域的含义分别为：秒、分钟、小时、日、月、周、年(可选)
+  通配符：
+    \* 表示所有值；
+    ? 表示未说明的值，即不关心它为何值；
+    \- 表示一个指定的范围；
+    , 表示附加一个可能值；
+    / 符号前表示开始时间，符号后表示每次递增的值；
+ */
+
+/**
+ * 自定义定时任务，实现订单状态定时处理
+ */
+@Component
+@Slf4j
+public class OrderTask {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 处理支付超时订单
+     * 每分钟一次
+     */
+    @Scheduled(cron = "0 * * * * ?")
+    public void processTimeoutOrder(){
+        log.info("处理支付超时订单：{}", new Date());
+
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-15);
+
+        // select * from orders where status = 1 and order_time < 当前时间-15分钟
+        List<Orders> ordersList = orderMapper.getByStatusAndOrdertimeLT(Orders.PENDING_PAYMENT, time);
+        if(ordersList != null && ordersList.size() > 0){
+            ordersList.forEach(order -> {
+                order.setStatus(Orders.CANCELLED);
+                order.setCancelReason("支付超时，自动取消");
+                order.setCancelTime(LocalDateTime.now());
+                orderMapper.update(order);
+            });
+        }
+    }
+
+    /**
+     * 处理“派送中”状态的订单
+     * 每天0100强行标记所有派送中为完成
+     */
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void processDeliveryOrder(){
+        log.info("处理派送中订单：{}", new Date());
+        // select * from orders where status = 4 and order_time < 当前时间-1小时
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-60);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrdertimeLT(Orders.DELIVERY_IN_PROGRESS, time);
+
+        if(ordersList != null && ordersList.size() > 0){
+            ordersList.forEach(order -> {
+                order.setStatus(Orders.COMPLETED);
+                orderMapper.update(order);
+            });
+        }
+    }
+
+}
